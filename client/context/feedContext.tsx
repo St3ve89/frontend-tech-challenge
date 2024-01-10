0c16a0a7fe246a909f6aab8cea304244a81786ea
St3ve89 <istvan.acs89@gmail.com>
2024-01-10 17:22:12 +0000

    Create a feed context

--- client/context/feedContext.tsx
+++ client/context/feedContext.tsx
@@ -1,16 +1,24 @@
 import { ReactNode, createContext, useEffect, useState } from 'react';
 import { Feed } from '../components/Feed/types';
+import { useInView } from '../hooks/useInView';
 
 export const FeedContext = createContext({
   feeds: [] as Feed[],
   isLoading: false,
+  fetchFeeds: () => {},
+  sentinelRef: (node?: Element | null | undefined) => {},
 });
 
 export const FeedProvider = ({ children }: { children: ReactNode }) => {
   const [feeds, setFeeds] = useState<Feed[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [hasMoreFeeds, setHasMoreFeeds] = useState(true);
 
-  const fetchFeeds = async (pageNumber = 1) => {
+  const { ref, inView } = useInView();
+
+  const fetchFeeds = async () => {
+    if (isLoading || !hasMoreFeeds) return;
     setIsLoading(true);
     try {
       const response = await fetch(
@@ -19,21 +27,35 @@ export const FeedProvider = ({ children }: { children: ReactNode }) => {
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
-      const data = await response.json();
-      setFeeds(data);
+      const newFeeds = await response.json();
+      setFeeds((currentFeeds) => [...currentFeeds, ...newFeeds]);
+      setPageNumber((currentPage) => currentPage + 1);
+      setHasMoreFeeds(newFeeds.length >= 5);
     } catch (error) {
       console.error('Error fetching feeds:', error);
-    } finally {
-      setIsLoading(false);
     }
+    setIsLoading(false);
   };
 
   useEffect(() => {
     fetchFeeds();
   }, []);
 
+  useEffect(() => {
+    if (inView) {
+      fetchFeeds();
+    }
+  }, [inView]);
+
   return (
-    <FeedContext.Provider value={{ feeds, isLoading }}>
+    <FeedContext.Provider
+      value={{
+        feeds,
+        isLoading,
+        fetchFeeds,
+        sentinelRef: ref,
+      }}
+    >
       {children}
     </FeedContext.Provider>
   );
