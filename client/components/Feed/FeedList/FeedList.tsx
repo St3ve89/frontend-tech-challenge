965395f6376f87f2a42395919eb65c9bda8ceae7
St3ve89 <istvan.acs89@gmail.com>
2024-01-10 17:22:26 +0000

    Refactor feedlist

--- client/components/Feed/FeedList/FeedList.tsx
+++ client/components/Feed/FeedList/FeedList.tsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from 'react';
 import { useFeed } from '../../../hooks/useFeed';
+import { FeedItem } from '../FeedItem';
+import { FeedListWrapper, FeedLoadMore } from './FeedList.style';
 
 export const FeedList = () => {
-  const { feeds, isLoading } = useFeed();
+  const { feeds, isLoading, sentinelRef } = useFeed();
+  const [initialLoadComplete, setInitialLoadComplete] = useState(false);
 
-  console.log('feeds:', feeds);
-  return <div>FeedList</div>;
+  useEffect(() => {
+    if (feeds.length > 0) {
+      setInitialLoadComplete(true);
+    }
+  }, [feeds.length]);
+
+  return (
+    <>
+      <FeedListWrapper>
+        {feeds.map((feed, idx) => (
+          <FeedItem data={feed} key={`${feed.briefref}_${idx}`} />
+        ))}
+      </FeedListWrapper>
+      {initialLoadComplete && (
+        <FeedLoadMore ref={sentinelRef}>
+          {isLoading && <p>loading more feeds...</p>}
+        </FeedLoadMore>
+      )}
+    </>
+  );
 };
